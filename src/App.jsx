971ce7feeff1bd886a7d5e9362535eb1cda417d1
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-03-01 01:02:21 -0300

    Criando header e estilio global

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Header } from './components/Header'
+
+import './global.css'
+
 export function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header />
+    </div>
   )
 }
